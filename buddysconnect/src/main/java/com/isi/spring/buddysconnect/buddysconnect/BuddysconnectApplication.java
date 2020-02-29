df4fdbd135aa4745fa3d4e3d599e27e8f7853c16
jkonkala <jkonkala@DLGJFT2.isimtl.local>
2020-02-29 12:32:42 -0500

    My first Commit

---
+++ buddysconnect/src/main/java/com/isi/spring/buddysconnect/buddysconnect/BuddysconnectApplication.java
@@ -0,0 +1,13 @@
+package com.isi.spring.buddysconnect.buddysconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddysconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddysconnectApplication.class, args);
+	}
+
+}
---
+++ buddysconnect/src/test/java/com/isi/spring/buddysconnect/buddysconnect/BuddysconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi.spring.buddysconnect.buddysconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddysconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
